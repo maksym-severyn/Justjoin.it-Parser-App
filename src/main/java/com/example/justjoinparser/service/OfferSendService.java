9cb5749216eeac0a5c3d57adc0cfddce4a238134
max <severinmaxim2015@gmail.com>
2023-06-25 21:19:20 +0200

    Revert "RabbitMQ: RPC request/reply pattern examples added"
    This reverts commit 1b0b8f690cb376aaac754ea1c6c33537ff8b447b.

--- src/main/java/com/example/justjoinparser/service/OfferSendService.java
+++ src/main/java/com/example/justjoinparser/service/OfferSendService.java
@@ -1,8 +1,6 @@
 package com.example.justjoinparser.service;
 
-import com.example.justjoinparser.amqp.publisher.GenericMessageBrokerPublisher;
 import com.example.justjoinparser.amqp.publisher.MessageBrokerPublisher;
-import com.example.justjoinparser.dto.OfferDto;
 import com.example.justjoinparser.filter.City;
 import com.example.justjoinparser.filter.PositionLevel;
 import com.example.justjoinparser.filter.Technology;
@@ -19,7 +17,6 @@ public class OfferSendService {
     private final PageService pageService;
     private final OfferService offerService;
     private final MessageBrokerPublisher messageBrokerPublisher;
-    private final GenericMessageBrokerPublisher<OfferDto> genericMessageBrokerPublisher;
 
     public void parseAndSend(PositionLevel positionLevel, City city, Technology technology) {
 //        pageService.parseOffers(positionLevel, city, technology)
@@ -30,20 +27,11 @@ public class OfferSendService {
 //            .onErrorContinue((error, cause) -> log.error("An error occurred during object handling", error))
 //            .subscribe();
 
-//        Flux<Object> offerDtoFluxToBeSent = pageService.parseOffers(positionLevel, city, technology)
-//            .flatMap(offerService::save);
-//
-//        messageBrokerPublisher.sendMessage("offers.exchange",
-//            "offers."+ city.getValue() + "." + technology.getValue(),
-//            offerDtoFluxToBeSent);
+        Flux<Object> offerDtoFluxToBeSent = pageService.parseOffers(positionLevel, city, technology)
+            .flatMap(offerService::save);
 
-        // RPC test
-        pageService.parseOffers(positionLevel, city, technology)
-            .flatMap(offerService::save)
-            .doOnNext(offerDto -> genericMessageBrokerPublisher.sendMessage(
-                "offers.exchange", "offers."+ city.getValue() + "." + technology.getValue(), offerDto)
-            )
-            .onErrorContinue((error, cause) -> log.error("An error occurred during object handling", error))
-            .subscribe();
+        messageBrokerPublisher.sendMessage("offers.exchange",
+            "offers."+ city.getValue() + "." + technology.getValue(),
+            offerDtoFluxToBeSent);
     }
 }
