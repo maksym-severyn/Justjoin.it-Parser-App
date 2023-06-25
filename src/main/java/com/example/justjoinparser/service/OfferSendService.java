bc88d0f7752596c464d54e6238b21d31e16c7bdd
max <severinmaxim2015@gmail.com>
2023-06-25 11:22:04 +0200

    OfferSendService added

---
+++ src/main/java/com/example/justjoinparser/service/OfferSendService.java
@@ -0,0 +1,35 @@
+package com.example.justjoinparser.service;
+
+import com.example.justjoinparser.amqp.MessageBrokerPublisher;
+import com.example.justjoinparser.filter.City;
+import com.example.justjoinparser.filter.PositionLevel;
+import com.example.justjoinparser.filter.Technology;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class OfferSendService {
+
+    private final PageService pageService;
+    private final OfferService offerService;
+    private final MessageBrokerPublisher messageBrokerPublisher;
+
+    public void parseAndSend(PositionLevel positionLevel, City city, Technology technology) {
+        pageService.parseOffers(positionLevel, city, technology)
+            .flatMap(offerService::save)
+            .doOnNext(offerDto -> messageBrokerPublisher.sendMessage(
+                "offers.exchange", "offers.wroclaw.java", offerDto)
+            )
+            .onErrorContinue((error, cause) -> log.error("An error occurred during object handling", error))
+            .subscribe();
+
+//        Flux<Object> offerDtoFlux = pageService.parseOffers(positionLevel, city, technology)
+//            .flatMap(offerService::save);
+//
+//        messageBrokerPublisher.sendMessage("offers.exchange", "offers.wroclaw.java", offerDtoFlux);
+
+    }
+}
