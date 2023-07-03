99752d09739a6ba10adb01b0b6a9a5c65c0c9f7e
max <severinmaxim2015@gmail.com>
2023-07-04 00:58:46 +0200

    Init scheduler added

--- src/main/java/com/example/justjoinparser/schedule/ParseOfferScheduler.java
+++ src/main/java/com/example/justjoinparser/schedule/ParseOfferScheduler.java
@@ -4,7 +4,9 @@ import com.example.justjoinparser.filter.City;
 import com.example.justjoinparser.filter.PositionLevel;
 import com.example.justjoinparser.filter.Technology;
 import com.example.justjoinparser.service.OfferSendService;
+import com.example.justjoinparser.service.OfferService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.core.LockAssert;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -14,25 +16,32 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class ParseOfferScheduler {
 
     private final OfferSendService offerSendService;
+    private final OfferService offerService;
 
     @EventListener(ApplicationReadyEvent.class)
-    @Scheduled(
-        cron = "${scheduler.parse-offers.java.mid.cron}",
-        zone = "${scheduler.parse-offers.java.mid.timezone}"
-    )
+    @SchedulerLock(name = "initJavaMidOffersScheduler_task", lockAtMostFor = "2m", lockAtLeastFor = "2m")
+    public void initJavaMidOffersScheduler() {
+        LockAssert.assertLocked();
+        offerService.count()
+                .doOnNext(size -> log.info("Currently there are {} offers in database", size))
+                .filter(size -> size == 0)
+                .doOnNext(size -> log.info("No offers found, initializing the scheduler for parsing and sending offers"))
+                .doOnError(error -> log.error("Error occurred while initializing scheduler: ", error))
+                .subscribe(size -> parseAndSendMidOffersScheduler(Technology.JAVA));
+    }
+
+    @Scheduled(cron = "${scheduler.parse-offers.java.mid.cron}", zone = "${scheduler.parse-offers.java.mid.timezone}")
     @SchedulerLock(name = "parseAndSendJavaMidOffersScheduler_task", lockAtMostFor = "15m", lockAtLeastFor = "5m")
     public void parseAndSendJavaMidOffersScheduler() {
         LockAssert.assertLocked();
         parseAndSendMidOffersScheduler(Technology.JAVA);
     }
 
-    @Scheduled(
-        cron = "${scheduler.parse-offers.python.mid.cron}",
-        zone = "${scheduler.parse-offers.python.mid.timezone}"
-    )
+    @Scheduled(cron = "${scheduler.parse-offers.python.mid.cron}", zone = "${scheduler.parse-offers.python.mid.timezone}")
     @SchedulerLock(name = "parseAndSendPythonMidOffersScheduler_task", lockAtMostFor = "15m", lockAtLeastFor = "5m")
     public void parseAndSendPythonMidOffersScheduler() {
         LockAssert.assertLocked();
